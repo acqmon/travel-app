36ee992c28448f7fdcb39608405e49ccff01af20
amonteiro <amonteiro030@gmail.com>
2026-03-27 17:57:20 +0530

    feat: introduce new models and define their associations in the database index.

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -1,5 +1,33 @@
 import User from "./user.model.js";
 import PartnerProfile from "./partner_profile.model.js";
 import BusinessType from "./business_type.model.js";
+import Activity from "./activity.model.js";
+import Category from "./category.model.js";
+import ActivityCategoryMapping from "./activity_category_mapping.model.js";
+import PartnerActivity from "./partner_activity.model.js";
 
-export { User, PartnerProfile, BusinessType };
+export {
+  User,
+  PartnerProfile,
+  BusinessType,
+  Activity,
+  Category,
+  ActivityCategoryMapping,
+  PartnerActivity,
+};
+
+User.hasOne(PartnerProfile, { foreignKey: "userId" });
+PartnerProfile.belongsTo(User, { foreignKey: "userId" });
+
+PartnerProfile.hasMany(PartnerActivity, { foreignKey: "partnerProfileId" });
+PartnerActivity.belongsTo(PartnerProfile, { foreignKey: "partnerProfileId" });
+PartnerProfile.belongsTo(BusinessType, { foreignKey: "businessTypeId" });
+
+Activity.hasMany(ActivityCategoryMapping, { foreignKey: "activityId" });
+
+Category.hasMany(ActivityCategoryMapping, { foreignKey: "categoryId" });
+
+BusinessType.hasMany(PartnerProfile, { foreignKey: "businessTypeId" });
+
+ActivityCategoryMapping.belongsTo(Activity, { foreignKey: "activityId" });
+ActivityCategoryMapping.belongsTo(Category, { foreignKey: "categoryId" });
